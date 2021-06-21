93153fbab95ed5682b63cf0b8159679cd569f1ec
Joao Soares <jpsoaresx@gmail.com>
2021-06-21 14:58:21 +0100

    adding script to upload packages from remote

--- create-package/upload-package-remote.sh
+++ create-package/upload-package-remote.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+# Downloads packages from SOURCE_PACKAGES_HOST and uploads them to HOST
 # Passwords are dummy passwords.
 
 # -------- CONFIG --------
 INPUT_FILE="./input/input-urls.txt"
 OUTPUT_TMP="./output_tmp"
-PACKAGES="https://drive.google.com/u/0/uc?id=1yWHbmeNEszDV8sFMsSffQbkZWRziNrX6&export=download"
-PACKAGE_NAME="author_packages"
+PACKAGE_NAME="a3_tranformed-pages-author"
 USERNAME="admin"
 PASSWORD="admin"
 PROTOCOL="http://"
 HOST="localhost"
 PORT="4502"
+NUMBER_OF_PACKAGES=1
+SOURCE_PACKAGES_PROTOCOL="http://"
+SOURCE_PACKAGES_HOST="aem-stg64-cms1.aws-preprod.telegraph.co.uk"
+SOURCE_PACKAGES_PORT="4502"
+SOURCE_PACKAGE_USERNAME="admin"
+SOURCE_PACKAGE_PASSWORD="admin"
 DELETE_MODE="false"
 SINGLE_PACKAGE_MODE="false"
 TEMP_FILE="tmp.txt"
@@ -52,13 +58,11 @@ processTmp () {
 	else
 		echo "---- START - processing batch $packageName -----"
 
-        cp "${OUTPUT_TMP}/tmp.txt" "${OUTPUT_TMP}/${packageName}.txt"
-        
-        printf "${packageName} uploaded\n"
+		cp "${OUTPUT_TMP}/tmp.txt" "${OUTPUT_TMP}/${packageName}.txt"
+		printf "${packageName} uploaded\n"
 		curl -u "${USERNAME}":"${PASSWORD}" -F file=@"${OUTPUT_TMP}/${packageName}".zip -F name="${packageName}" -F force=true -F install=false ${PROTOCOL}${HOST}:${PORT}/crx/packmgr/service.jsp
 		printf "\n"
-
-        printf "${packageName} installed\n"
+		printf "${packageName} installed\n"
 		curl -u "${USERNAME}":"${PASSWORD}" -F file=@"${OUTPUT_TMP}/${packageName}".zip -F name="${packageName}" -F force=true -F install=true ${PROTOCOL}${HOST}:${PORT}/crx/packmgr/service.jsp
 		printf "\n"
 
@@ -70,17 +74,20 @@ processTmp () {
 
 # ------ MAIN --------
 rm -rf ${OUTPUT_TMP}
-echo "Getting list of packages from ${PACKAGES}"
-curl -s -L --create-dirs -o ${OUTPUT_TMP}/packages.csv ${PACKAGES}
-counter=0  
-while read line
-do
-	curl -s -L --create-dirs -o ${OUTPUT_TMP}/${PACKAGE_NAME}-${counter}.zip ${line}
-	echo "Dowloaded ${line} as ${PACKAGE_NAME}-${counter}.zip"
-	let counter++
-done < ${OUTPUT_TMP}/packages.csv
 
-for (( c=0; c<counter; c++ ))
+for (( c=1; c <= NUMBER_OF_PACKAGES; c++ ))
+do
+	url="${SOURCE_PACKAGES_PROTOCOL}${SOURCE_PACKAGES_HOST}:${SOURCE_PACKAGES_PORT}/crx/packmgr/download.jsp?_charset_=utf-8&path=/etc/packages/my_packages/${PACKAGE_NAME}-${c}.zip"
+	code=$(curl  -u "${SOURCE_PACKAGE_USERNAME}":"${SOURCE_PACKAGE_PASSWORD}" -f -L -w '%{http_code}' --create-dirs -o ${OUTPUT_TMP}/${PACKAGE_NAME}-${c}.zip ${url})
+	if [[ "$code" =~ ^2 ]]; then
+		echo "Dowloaded ${PACKAGE_NAME}-${c}.zip"
+	else
+		echo "There was an issue downloading the files"
+		exit $code
+	fi
+done
+
+for (( c=1; c <= NUMBER_OF_PACKAGES; c++ ))
 do
    processTmp ${c}
 done
